f430c801dfecf38ea2bbdaadb31c2832e90f2f5f
Sam Stiles <sam@phasesolutions.ca>
2015-02-21 18:57:31 -0400

    Tweaks to dummy data

--- api/controllers/SubmissionController.js
+++ api/controllers/SubmissionController.js
@@ -15,34 +15,45 @@ module.exports = {
     var UnemploymentProvince = UnemploymentRatesStatsCan.find().where({ year: 2012, province: province });
  
     q.props({GradSurvey: GradSurvey, UnemploymentOntario: UnemploymentOntario, UnemploymentProvince: UnemploymentProvince}).then(function(results) {
-      var gradRateUnadjusted = results.GradSurvey[0]['employment6mo'];
-      var ontarioGradRate = 100 - results.UnemploymentOntario[0]['university'] == null ? null : 100 - results.UnemploymentOntario[0]['university'];
-      var provinceGradRate = results.UnemploymentProvince[0]['university'] == null ? null : 100 - results.UnemploymentProvince[0]['university'];
+      var gradRateUnadjusted = results.GradSurvey[0].employment6mo;
       var gradRateAdjusted = getAdjustedRate(gradRateUnadjusted, ontarioGradRate, provinceGradRate);
-      var nonGradRate = 100 - results.UnemploymentProvince[0]['highSchool'] == null ? null : 100 - results.UnemploymentProvince[0]['highSchool'];
+      var ontarioGradRate = 100 - results.UnemploymentOntario[0].university === null ? null : 100 - results.UnemploymentOntario[0].university;
+      var provinceGradRate = results.UnemploymentProvince[0].university === null ? null : 100 - results.UnemploymentProvince[0].university;
+      var nonGradRate = 100 - results.UnemploymentProvince[0].highSchool === null ? null : 100 - results.UnemploymentProvince[0].highSchool;
 
       results = {
+
+        // The users choice
         'selectedGrad': {
           'employmentRate': gradRateAdjusted,
           'breakEven': 5.2,
-          'salary': 48000,
+          'salary': 48000
         },
+
+        // If the user does not go to school
         'noGrad': {
           'employmentRate': nonGradRate,
-          'salary': 25000,
+          'salary': 25000
         },
+
         'alternativeGrad': [
+
+          // Alternate but similar career choice #1
           {
-          'employmentRate': 97.2,
-          'breakEven': 3,
-          'salary': 62000,
+            'name': 'Law',
+            'employmentRate': 97.2,
+            'breakEven': 3,
+            'salary': 62000
           },
+
+          // Alternate but similar career choice #2
           {
-          'employmentRate': 96.7,
-          'breakEven': 4.6,
-          'salary': 59000,
-          },
-        ],
+            'name': 'Optometry',
+            'employmentRate': 96.7,
+            'breakEven': 4.6,
+            'salary': 59000
+          }
+        ]
       };
 
       return res.send(results);
