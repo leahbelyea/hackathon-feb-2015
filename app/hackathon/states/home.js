9d1d21806c8d1c7eeb9fdf1b65a295d22021d633
Sam Stiles <sam@phasesolutions.ca>
2015-02-21 14:36:59 -0400

    <md-select /> apparently isn't a thing yet

--- app/hackathon/states/home.js
+++ app/hackathon/states/home.js
@@ -12,33 +12,24 @@ Hackathon.config(['$stateProvider',
       },
       controller: ['thing', '$q', '$http', '$timeout', function(thing, $q, $http, $timeout) {
         var ctrl = this;
-        ctrl.thing = thing;
-        
-        ctrl.loadProvinceList = function() {
-          console.log('Test');
-          ctrl.provinces = [];
-          var deferred = $q.defer();
-          $http.get('/api/provinceList')
-          .success(function(data, status) {
-            console.log('Thing');
-            ctrl.provinces = data;
-            deferred.resolve();
-          });
-          return deferred;
-        };
 
-        ctrl.loadUsers = function() {
-          console.log('Testtt');
-          ctrl.users = [];
-          return $timeout(function() {
-            ctrl.users = [
-              { id: 1, name: 'Scooby Doo' },
-              { id: 2, name: 'Shaggy Rodgers' },
-              { id: 3, name: 'Fred Jones' },
-              { id: 4, name: 'Daphne Blake' },
-              { id: 5, name: 'Velma Dinkley' },
-            ];
-          }, 650);
+        ctrl.thing = thing;
+
+        ctrl.provinces = [
+          {'name': 'Alberta',                   'abbreviation': 'AB' },
+          {'name': 'British Columbia',          'abbreviation': 'BC' },
+          {'name': 'Manitoba',                  'abbreviation': 'MB' },
+          {'name': 'New Brunswick',             'abbreviation': 'NB' },
+          {'name': 'Newfoundland and Labrador', 'abbreviation': 'NL' },
+          {'name': 'Nova Scotia',               'abbreviation': 'NS' },
+          {'name': 'Ontario',                   'abbreviation': 'ON' },
+          {'name': 'Prince Edward Island',      'abbreviation': 'PE' },
+          {'name': 'Quebec',                    'abbreviation': 'PQ' },
+          {'name': 'Saskatchewan',              'abbreviation': 'SK' }
+        ];
+
+        ctrl.selections = {
+          province: ''
         };
         
         window.HomeController = ctrl;
