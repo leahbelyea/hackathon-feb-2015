7f67083a32d853c91ee49f53143e23e6a98647be
Sam Stiles <sam@phasesolutions.ca>
2015-02-21 11:54:48 -0400

    Route conflict resolved

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,8 @@
 module.exports.routes = {
 
-<<<<<<< HEAD
   'POST /api/submit': {
     controller: 'SubmissionController',
     action: 'submit'
   }
 
 };
-=======
-  /**
-   * GET (read) ALL routes
-   */
-};
->>>>>>> added models for datasets
