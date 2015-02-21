73ebc87d1e3537ffe50746f4778d8ff72fbc3de3
Sam Stiles <sam@phasesolutions.ca>
2015-02-21 18:07:38 -0400

    Misc frontend changes and retrieving the list of valid industries

--- api/controllers/SubmissionController.js
+++ api/controllers/SubmissionController.js
@@ -8,6 +8,8 @@ module.exports = {
     var name = req.param('name');
     var province = req.param('province') == 'Other' ? 'Canada' : req.param('province');
 
+    console.log(req.params.all());
+
     var GradSurvey = OntarioGradSurvey.find().where({ programArea: industry});
     var UnemploymentSC = UnemploymentRatesStatsCan.find().where({ year: 2012, province: province });
  
@@ -17,6 +19,14 @@ module.exports = {
     }).catch(function(error) {
       console.log(error);
       return res.send(200);
+    });
+  },
+
+  getIndustryList: function(req, res) {
+    SimilarPrograms.find().exec(function(err, programs) {
+      if (err || !programs) return res.send(500, { error: err, programs: programs });
+
+      return res.send(programs);
     });
   }
 
--- app/hackathon/states/home.js
+++ app/hackathon/states/home.js
@@ -6,12 +6,20 @@ Hackathon.config(['$stateProvider',
       templateUrl: '/hackathon/states/home.template.html',
       controllerAs: 'HomeCtrl',
       resolve: {
-        "thing": function() {
-          return true;
-        }
+        "industries": ['$q', function($q) {
+          var deferred = $q.defer();
+          $http.get('/api/getIndustryList')
+          .success(function(data, status) {
+            var list = _.keys(data);
+            deferred.resolve(list);
+          });
+          return deferred;
+        }]
       },
-      controller: ['$http', function($http) {
+      controller: ['$http', '$mdDialog', 'industries', function($http, $mdDialog, industries) {
         var ctrl = this;
+
+        ctrl.errs = [];
         ctrl.provinces = ['Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador', 'Nova Scotia', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan'];
         ctrl.industries = ['Business', 'Arts', 'Science', 'Law'];
         ctrl.loading = false;
@@ -22,8 +30,41 @@ Hackathon.config(['$stateProvider',
           gender: ''
         };
 
+        console.log(ctrl.industries);
+
+        ctrl.errorsOnForm = function() {
+          $mdDialog.show(
+            $mdDialog.alert()
+              .content('All fields in the form a required!')
+              .ok('Got it!')
+          );
+        };
+
         ctrl.submit = function() {
-          $http.post('/api/submit')
+          ctrl.errs = [];
+
+          if (ctrl.selections.province === '') {
+            ctrl.errs.push('Province is a required field!');
+          }
+
+          if (ctrl.selections.industry === '') {
+            ctrl.errs.push('Area of study is a required field!');
+          }
+
+          if (ctrl.selections.name === '') {
+            ctrl.errs.push('Name is a required field!');
+          }
+
+          if (ctrl.selections.gender === '') {
+            ctrl.errs.push('Gender choice is required!');
+          }
+
+          if (errs.length !== 0) {
+            return ctrl.errorsOnForm();
+          }
+
+          ctrl.loading = true;
+          $http.post('/api/submit', ctrl.selections)
           .success(function(data, status) {
             console.log('Success!');
           })
