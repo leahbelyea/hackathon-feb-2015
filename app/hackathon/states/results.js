2ff420e5e5de504f036e7bb01951a512450b3927
Sam Stiles <sam@phasesolutions.ca>
2015-02-22 14:40:39 -0400

    Remove dummy data

--- app/hackathon/states/results.js
+++ app/hackathon/states/results.js
@@ -7,37 +7,6 @@ Hackathon.config(['$stateProvider',
       controllerAs: 'ResultsCtrl',
       resolve: {
         'results': ['$rootScope', '$q', function($rootScope, $q) {
-          return {
-            name: 'Leah',
-            gender: 'Female',
-            chosenIndustry: {
-              name: 'Optometry',
-              salary6mo: '~$60,000',
-              debtIncurred: '~$15,543',
-              employmentRate: '~96%',
-              breakEven: '~3.5'
-            },
-            noDegree: {
-              salary: '~$25,000',
-              employmentRate: '~78%',
-              debtIncurred: '~$0',
-              netWorth10yrs: '~$1,000,000'
-            },
-            alternateIndustry1: {
-              name: 'Medicine',
-              salary6mo: '~$78,000',
-              debtIncurred: '~$17,543',
-              employmentRate: '~100%',
-              breakEven: '~2.5'
-            },
-            alternateIndustry2: {
-              name: 'Optometry',
-              salary6mo: '~$66,000',
-              debtIncurred: '~$13,543',
-              employmentRate: '~100%',
-              breakEven: '~3'
-            }
-          };
           var deferred = $q.defer();
           if ($rootScope.results) deferred.resolve($rootScope.results);
           else deferred.reject('You need to have submitted the form on step 1 first.');
